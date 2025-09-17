03d4d498f49b04f455f8ef27fdc229169c32d9bd
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2025-09-17 13:38:25 +0200

    SPHM-1352 - HmIP-DLP(-A) - Status control DOOR_LOCK_TRANSCEIVER (chn. 12) - Layout changed and a info dialog added. Some translations are still missing.

--- src/webui/www_source/ise/js/iseDoorLockTransceiver.js
+++ src/webui/www_source/ise/js/iseDoorLockTransceiver.js
@@ -15,11 +15,17 @@ iseDoorLockTransceiver.prototype = {
   initElem: function() {
     var self = this;
     this.btnLockTargetLevel = jQuery("#"+this.id+"lockTargetLevel");
+    this.btnShowInfoPanel = jQuery("#"+this.id+"showDLPInfoPanel");
 
     this.btnLockTargetLevel.on("click", function() {
       self.getDialog();
       self.selOptionElem = jQuery("#"+self.id+"selOptionElem");
     });
+
+    this.btnShowInfoPanel.on("click", function() {
+      self.getInfoPanel();
+    });
+
   },
   
   getDialog: function() {
@@ -37,6 +43,18 @@ iseDoorLockTransceiver.prototype = {
     dlg.btnTextNo(translateKey("dialogBack"));
     dlg.btnTextYes(translateKey("btnOk"));
 
+  },
+
+  getInfoPanel: function() {
+    var self = this,
+      content = this.getInfoPanelHtml();
+
+    var dlg = new YesNoDialog(translateKey("dialogInfo"), content, function(result) {
+
+    },"html");
+    dlg.btnNoHide();
+    dlg.btnTextYes(translateKey("btnOk"));
+    dlg.setWidth(((parseInt(self.opts.infoTeachInState) > 4) && (parseInt(self.opts.infoTeachInState) < 10)) ? 800 : 600); // for very long Messages we increase the size of the dialog.
   },
 
   getDialogHtml: function() {
@@ -60,5 +78,40 @@ iseDoorLockTransceiver.prototype = {
     html += "</table>";
 
     return html;
+  },
+
+  getInfoPanelHtml: function() {
+    var html = "";
+
+    html += "<table align='center'>";
+
+      html += "<tr>";
+        html += "<td>";
+          html += "<div class='ControlBtnInfo' style='line-height:50px;'>${dlp_LOCK_STATE}: ${dlpLockState_"+this.opts.infoLockState+"}</div>"
+        html += "</td>";
+      html += "</tr>";
+
+      html += "<tr>";
+        html += "<td>";
+          html += "<div class='ControlBtnInfo' style='line-height:50px;'>${dlp_LOCK_STATE_REASON}: ${dlpLockStateReason_"+this.opts.infoReason+"}</div>"
+        html += "</td>";
+      html += "</tr>";
+
+      html += "<tr>";
+        html += "<td>";
+          html += "<div class='ControlBtnInfo' style='line-height:50px;'>${dlp_LOCK_TEACH_IN_STATE}: ${dlpLockTeachInState_"+this.opts.infoTeachInState+"}</div>"
+        html += "</td>";
+      html += "</tr>";
+
+      html += "<tr>";
+        html += "<td>";
+         html += "<div class='ControlBtnInfo' style='line-height:50px;'>${dlp_LAST_LOCK_DRIVE_LOAD}: "+this.opts.infoLastLockDriveLoad+"</div>"
+        html += "</td>";
+      html += "</tr>";
+
+    html += "</table>";
+
+    return html;
   }
+
 };

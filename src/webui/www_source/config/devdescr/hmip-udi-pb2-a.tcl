55d56a3f5d4be87bfb4ae160b690a257a8bc48c2
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2025-09-24 13:22:02 +0200

    SPHM-1426 - HmIP-UDI-PB2(-A) - Preview picture

--- src/webui/www_source/config/devdescr/hmip-udi-pb2-a.tcl
+++ src/webui/www_source/config/devdescr/hmip-udi-pb2-a.tcl
@@ -15,24 +15,6 @@ set     PATHLIST ""
 lappend PATHLIST [list  50	"/config/img/devices/50/254_hmip-udi-pb2_thumb.png"]
 lappend PATHLIST [list  250	"/config/img/devices/250/254_hmip-udi-pb2.png"]
 
-#Koordinaten für Highlight:
-#P {<Formname> <Formtyp> <x,y,dx,dy,r je nach Formtyp>}
-#-----------------------------------------------------------------------
-
-#Beispiel Circle: varname kreistyp x y r
-#lappend P {"4"   1 380 475 170}
-#Beispiel Rectangle varname rechtecktyp x y dx dy
-#lappend P {"1+2" 2 187 202 380 302}
-
 
 set P ""
 
-lappend P {"Icon" 3 0.092 0.6 'Icon_folgt' 0.14 'verdana' Font.BOLD}
-
-#x: 110 y: 58 fontsize: 45
-lappend P {"1_channel" 3 0.44 0.232 '1' 0.18 'verdana' Font.BOLD}
-lappend P {"1" 5 '1_channel' 'Icon'}
-
-
-lappend P {"2_channel" 3 0.44 0.232 '2' 0.18 'verdana' Font.BOLD}
-lappend P {"2" 5 '2_channel' 'Icon'}

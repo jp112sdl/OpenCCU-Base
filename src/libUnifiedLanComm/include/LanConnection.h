c81e37a6c0ab1838cbb51135c8af64d08f940d29
cn-eq3 <211333494+cn-eq3@users.noreply.github.com>
2026-01-15 15:05:13 +0100

    libUnifiedLanComm: Add license header

--- src/libUnifiedLanComm/include/LanConnection.h
+++ src/libUnifiedLanComm/include/LanConnection.h
@@ -1,3 +1,11 @@
+/*
+* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* 
+* Licensed under the HMSL 2 (the "License"). You may not use
+* this file except in compliance with the License.  You can obtain a copy
+* in the file HMSL.txt in the source distribution.
+*/
+
 #ifndef _LANCONNECTION_H_
 #define _LANCONNECTION_H_
 
--- src/libUnifiedLanComm/test/src/TestCommonConversion.h
+++ src/libUnifiedLanComm/test/src/TestCommonConversion.h
@@ -1,10 +1,10 @@
 /*
- * TestCommonConversion.cpp
- *
- *  Created on: May 31, 2013
- *      Author: niclaus
- */
-
+* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* 
+* Licensed under the HMSL 2 (the "License"). You may not use
+* this file except in compliance with the License.  You can obtain a copy
+* in the file HMSL.txt in the source distribution.
+*/
 
 //gtest stuff
 #include "gtest/gtest.h"

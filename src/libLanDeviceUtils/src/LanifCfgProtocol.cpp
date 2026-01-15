4fe4c54416afac85928080244bf3ed84827d8fba
cn-eq3 <211333494+cn-eq3@users.noreply.github.com>
2026-01-15 15:44:36 +0100

    libLanDeviceUtils: Add license header

--- src/libLanDeviceUtils/src/LanifCfgProtocol.cpp
+++ src/libLanDeviceUtils/src/LanifCfgProtocol.cpp
@@ -1,3 +1,11 @@
+/*
+* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* 
+* Licensed under the HMSL 2 (the "License"). You may not use
+* this file except in compliance with the License.  You can obtain a copy
+* in the file HMSL.txt in the source distribution.
+*/
+
 #include "LanifCfgProtocol.h"
 #include <InternalUtilities.h>
 #include <Logger.h>
--- src/libLanDeviceUtils/src/Protocol.h
+++ src/libLanDeviceUtils/src/Protocol.h
@@ -1,3 +1,11 @@
+/*
+* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* 
+* Licensed under the HMSL 2 (the "License"). You may not use
+* this file except in compliance with the License.  You can obtain a copy
+* in the file HMSL.txt in the source distribution.
+*/
+
 //#pragma once
 #ifndef _LIBLANDEVICEUTILS_PROTOCOL_H
 #define _LIBLANDEVICEUTILS_PROTOCOL_H

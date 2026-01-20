b0728ca0c23001d265d4062a7e9e80205d627bc8
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2026-01-20 11:25:06 +0100

    SPHM-1459 - ELV-SH-DUSI - Unnecessary stuff removed

--- src/webui/www_source/config/devdescr/elv-sh-dusi.tcl
+++ src/webui/www_source/config/devdescr/elv-sh-dusi.tcl
@@ -16,13 +16,3 @@ lappend PATHLIST [list  50	"/config/img/devices/50/259_elv-sh-dusi_thumb.png"]
 lappend PATHLIST [list  250	"/config/img/devices/250/259_elv-sh-dusi.png"]
 
 set P ""
-
-lappend P {"Icon" 3 0.092 0.6 'Icon_folgt' 0.14 'verdana' Font.BOLD}
-
-#x: 110 y: 58 fontsize: 45
-lappend P {"1_channel" 3 0.44 0.232 '1' 0.18 'verdana' Font.BOLD}
-lappend P {"1" 5 '1_channel' 'Icon'}
-
-
-lappend P {"2_channel" 3 0.44 0.232 '2' 0.18 'verdana' Font.BOLD}
-lappend P {"2" 5 '2_channel' 'Icon'}

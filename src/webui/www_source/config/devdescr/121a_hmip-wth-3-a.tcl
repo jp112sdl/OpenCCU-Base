7c3dd764df33227c291c95d3b218cbdfd254409d
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2026-01-22 08:43:14 +0100

    SPHM-1476 - HmIP-WTH-3(-A) -Unnecessary stuff removed

--- src/webui/www_source/config/devdescr/121a_hmip-wth-3-a.tcl
+++ src/webui/www_source/config/devdescr/121a_hmip-wth-3-a.tcl
@@ -15,14 +15,4 @@ set     PATHLIST ""
 lappend PATHLIST [list  50	"/config/img/devices/50/121_hmip-wth_thumb.png"]
 lappend PATHLIST [list  250	"/config/img/devices/250/121_hmip-wth.png"]
 
-#Koordinaten für Highlight:
-#P {<Formname> <Formtyp> <x,y,dx,dy,r je nach Formtyp>}
-#-----------------------------------------------------------------------
-
-#Beispiel Circle: varname kreistyp x y r
-#lappend P {"4"   1 380 475 170}
-#Beispiel Rectangle varname rechtecktyp x y dx dy
-#lappend P {"1+2" 2 187 202 380 302}
-
-
 set P ""

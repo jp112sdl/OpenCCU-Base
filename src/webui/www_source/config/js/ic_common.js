71586149637559bb4299ceb912118c3441f98064
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2026-04-29 14:42:45 +0200

    A fixed 'error' wasn't an error :-)

--- src/webui/www_source/config/js/ic_common.js
+++ src/webui/www_source/config/js/ic_common.js
@@ -22,7 +22,7 @@ AddProfileValues = function(prefix)
   var i = 1;
 
   var elem;
-  while (elem == document.getElementById(prefix + i))
+  while (elem = document.getElementById(prefix + i))
   {
     AddParam(elem);
     i++;
@@ -33,7 +33,7 @@ AddSeparateSettings = function(prefix, pnr)
 {
   var i = 1,
   elem;
-  while (elem == document.getElementById(prefix + pnr + '_' + i))
+  while (elem = document.getElementById(prefix + pnr + '_' + i))
   {
     // add ALL parameters (do NOT use IsDirty()) because
     // AddSeparateSettings is also used while modifiying

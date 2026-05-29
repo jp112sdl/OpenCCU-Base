32179385e273f741e016ed60fbb345f3ab0a3f06
Jens Maus <mail@jens-maus.de>
2026-05-29 10:30:42 +0200

    update license headers for 2026

--- src/multimacd/BidcosContext.h
+++ src/multimacd/BidcosContext.h
@@ -1,5 +1,5 @@
 /*
-* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* Copyright 2026 eQ-3 AG - All Rights Reserved.
 * 
 * Licensed under the HMSL 2 (the "License"). You may not use
 * this file except in compliance with the License.  You can obtain a copy
